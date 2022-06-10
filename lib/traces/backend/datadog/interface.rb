c37f19a199e744bf91c41f89cb106e703dccd0e8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-06-10 15:39:02 +1200

    Rework datadog requires.

--- lib/traces/backend/datadog/interface.rb
+++ lib/traces/backend/datadog/interface.rb
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'ddtrace'
+require 'datadog/tracing'
 
 require 'traces/context'
 require_relative 'version'
